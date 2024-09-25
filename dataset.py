e5d8bc4af065744cd015085203e5e10d22dac7c1
JAGADESH R <113334797+Jagadesh17@users.noreply.github.com>
2024-09-25 20:31:15 +0530

    Delete dataset.py

--- dataset.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_dataset(filepath='fraudTest.csv'):
-    data = pd.read_csv(filepath)
-    return data
-
-if __name__ == "__main__":
-    dataset = load_dataset()
-    print(dataset.head())
